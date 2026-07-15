9b34586adc3364c6b0cfef9cd2c62503539b0634
Kittisak 'Froy' Porkha <kittijustsaid@gmail.com>
2026-07-15 11:17:22 +0700

    update

--- unit_06/main.cpp
+++ unit_06/main.cpp
@@ -1,3 +1,5 @@
+#include <cassert>
+#include <cstdint>
 #include <iostream>
 // #include <iostream>
 
@@ -10,7 +12,7 @@ namespace operators {}
 //
 //   int x{}; // if you run your code in put it as 0, it's going to be error;
 //   because, your pc remember it as 0.0 std::cin >> x;
-//
+// jjj
 //   std::cout << "each person gets " << apples / x << " whole apples.\n";
 //
 //   return 0;
@@ -18,21 +20,44 @@ namespace operators {}
 //
 // 6.3 -- Remainder and exponentiation.
 
+// int main() {
+//   std::cout << "Enter an integer: ";
+//   int x{};
+//   std::cin >> x;
+//
+//   std::cout << "Enter another integer: ";
+//   int y{};
+//   std::cin >> y;
+//
+//   std::cout << "The remainder is: " << x % y << std::endl;
+//
+//   if ((x % y) == 0)
+//     std::cout << x << " in evenly divisible by " << y << '\n';
+//   else
+//     std::cout << x << " is not evenly divisible by " << y << '\n';
+//
+//   return 0;
+// }
+
+constexpr std::int64_t powint(std::int64_t base, int exp) {
+
+  assert(exp >= 0 && "powint: exp parameter has negative value");
+
+  if (base == 0)
+    return (exp == 0) ? 1 : 0;
+
+  std::int64_t result{1};
+  while (exp > 0) {
+    if (exp & 1)
+      result *= base;
+
+    exp /= 2;
+    base *= base;
+  }
+  return result;
+}
+
 int main() {
-  std::cout << "Enter an integer: ";
-  int x{};
-  std::cin >> x;
-
-  std::cout << "Enter another integer: ";
-  int y{};
-  std::cin >> y;
-
-  std::cout << "The remainder is: " << x % y << std::endl;
-
-  if ((x % y) == 0)
-    std::cout << x << " in evenly divisible by " << y << '\n';
-  else
-    std::cout << x << " is not evenly divisible by " << y << '\n';
-
+  std::cout << powing(7, 12) << std::endl;
   return 0;
 }
